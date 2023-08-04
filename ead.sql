b9bf54a2dc22ffc344ec076dfc4d764d6e3182cd
= <=>
2023-08-04 14:00:04 -0300

    fix: correcao do arquivo dump do banco de dados

--- ead.sql
+++ ead.sql
@@ -14,19 +14,100 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura do banco de dados para ead
+CREATE DATABASE IF NOT EXISTS `ead` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ead`;
+
+-- Copiando estrutura para tabela ead.alunos
+CREATE TABLE IF NOT EXISTS `alunos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `senha` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela ead.aluno_curso
+CREATE TABLE IF NOT EXISTS `aluno_curso` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_curso` int NOT NULL DEFAULT '0',
+  `id_aluno` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela ead.aulas
+CREATE TABLE IF NOT EXISTS `aulas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_modulo` int NOT NULL DEFAULT '0',
+  `id_curso` int NOT NULL DEFAULT '0',
+  `ordem` int NOT NULL DEFAULT '0',
+  `tipo` varchar(20) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
+-- Copiando estrutura para tabela ead.cursos
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `imagem` varchar(37) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `descricao` text COLLATE utf8mb4_general_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela ead.historico
+CREATE TABLE IF NOT EXISTS `historico` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `data_viewed` datetime NOT NULL,
+  `id_aluno` int NOT NULL DEFAULT '0',
+  `id_aula` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela ead.modulos
+CREATE TABLE IF NOT EXISTS `modulos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_curso` int NOT NULL DEFAULT '0',
+  `nome` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela ead.questionarios
+CREATE TABLE IF NOT EXISTS `questionarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_aula` int NOT NULL,
+  `pergunta` varchar(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `opcao1` varchar(100) COLLATE utf8mb4_general_ci DEFAULT '',
+  `opcao2` varchar(100) COLLATE utf8mb4_general_ci DEFAULT '',
+  `opcao3` varchar(100) COLLATE utf8mb4_general_ci DEFAULT '',
+  `opcao4` varchar(100) COLLATE utf8mb4_general_ci DEFAULT '',
+  `resposta` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela ead.videos
+CREATE TABLE IF NOT EXISTS `videos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_aula` int NOT NULL DEFAULT '0',
+  `nome` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `descricao` text COLLATE utf8mb4_general_ci,
+  `url` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
